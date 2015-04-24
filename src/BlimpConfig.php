92d775eb83c2c00f953d8bbd6246d5a1239e4682
Nelson Antunes <nelson.antunes@gmail.com>
2015-04-24 01:02:24 +0100

    dev version 0.1.5

--- src/BlimpConfig.php
+++ src/BlimpConfig.php
@@ -9,7 +9,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
  * Based in: https://github.com/doctrine/DoctrineMongoDBBundle/blob/f01a2ac4ca695356d25562598e3400652f037cff/DependencyInjection/Configuration.php
 */
 class BlimpConfig implements ConfigurationInterface {
-    private $api;
+    protected $api;
 
     public function __construct($api) {
         $this->api = $api;
